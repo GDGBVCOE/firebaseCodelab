fc20cd3e42dac6a8f73aef3c171b0ed85df178d5
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-05-20 01:15:23 +0530

    completed step 3 , tested on devices working properly

--- app/src/main/java/com/example/vaibhavchellani/firebaseioextended/MainActivity.java
+++ app/src/main/java/com/example/vaibhavchellani/firebaseioextended/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vaibhavchellani.firebaseioextended;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,8 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private Button sendButton;
     private ListView messageListView;
     private listviewAdapter mlistviewAdapter;
+    private String mUsername="ANONYMOUS";
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,33 +43,23 @@ public class MainActivity extends AppCompatActivity {
         messageEditText=(EditText) findViewById(R.id.messageEditText);
         sendButton=(Button) findViewById(R.id.sendButton);
         messageListView=(ListView)findViewById(R.id.messageListView);
-        List<Message> messages=new ArrayList<Message>();
+        final List<Message> messages=new ArrayList<Message>();
         Message newMessage=new Message();
-
-        //for testing listview
-        /*newMessage.setUserMessage("fdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfd");
-        newMessage.setUserName("vaibhav");
-        messages.add(newMessage);
-        messages.add(newMessage);messages.add(newMessage);messages.add(newMessage);*/
-
-
         mlistviewAdapter=new listviewAdapter(this,R.layout.row_layout,messages);
         messageListView.setAdapter(mlistviewAdapter);
 
-        // Send button listener
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        sendButton.setEnabled(false);
 
-            }
-        });
+
+        //getting reference to database
+        mDatabaseReference= FirebaseDatabase.getInstance().getReference();
+
 
         // if edit text is empty this disables the send button .
         messageEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
-
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.toString().trim().length() > 0) {
@@ -63,21 +68,26 @@ public class MainActivity extends AppCompatActivity {
                     sendButton.setEnabled(false);
                 }
             }
-
             @Override
             public void afterTextChanged(Editable editable) {
             }
         });
 
+        // Send button listener
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message addNewMessage=new Message();
+                addNewMessage.setUserName(mUsername+" : ");
+                addNewMessage.setUserMessage(messageEditText.getText().toString());
 
+                //the below line replaces existing data in the database , but we have to append to the list not replace
+                //mDatabaseReference.child("messages").setValue(addNewMessage);
 
+                mDatabaseReference.child("messages").push().setValue(addNewMessage);
 
-
-
-
-
-
-
-
+                messageEditText.setText("");
+            }
+        });
     }
 }
