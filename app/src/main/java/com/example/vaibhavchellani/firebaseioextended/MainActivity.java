c0ec28ecd8e37c01d9b071d55ac8c2e119ab1fc4
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-05-26 20:28:29 +0530

    remote config added

--- app/src/main/java/com/example/vaibhavchellani/firebaseioextended/MainActivity.java
+++ app/src/main/java/com/example/vaibhavchellani/firebaseioextended/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -18,8 +21,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -27,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText messageEditText;
     private Button sendButton;
+
+    private FirebaseRemoteConfig mRemoteConfig=FirebaseRemoteConfig.getInstance();
+
     private ListView messageListView;
     private listviewAdapter mlistviewAdapter;
     private String mUsername="ANONYMOUS";
@@ -49,6 +58,32 @@ public class MainActivity extends AppCompatActivity {
         messageListView.setAdapter(mlistviewAdapter);
 
         sendButton.setEnabled(false);
+
+        //STEP 1.  configure remote config to developer mode
+        mRemoteConfig.setConfigSettings(new FirebaseRemoteConfigSettings.Builder().
+                setDeveloperModeEnabled(true)
+                .build()
+        );
+
+        //STEP 2. set Default Values
+        HashMap<String,Object> defaults=new HashMap<>();
+        //setting message lenght to a default of 10
+        defaults.put("max_message_length",10);
+        mRemoteConfig.setDefaults(defaults);
+
+        //STEP 3 . use fetch to download defaults from console
+        final Task<Void> fetch = mRemoteConfig.fetch(0);
+        fetch.addOnSuccessListener(this, new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                mRemoteConfig.activateFetched();
+                updateMaxTextLength();
+            }
+        });
+
+
+
+        updateMaxTextLength();
 
 
         //getting reference to database
@@ -97,26 +132,24 @@ public class MainActivity extends AppCompatActivity {
                 messages.add(newmessage);
                 mlistviewAdapter.notifyDataSetChanged();
             }
-
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
             }
-
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-
             }
-
             @Override
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
+    }
+
+    private void updateMaxTextLength(){
+        int maxlength=(int) mRemoteConfig.getLong("max_message_length");
+        /*messageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(10)})*/;
+        messageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxlength)});
     }
 }
